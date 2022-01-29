e4d7e3541681613187265b6befa462595b8ba1a4
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-29 16:08:28 +0300

    added more content the Procfile and update txt file

--- app/views.py
+++ app/views.py
@@ -3,9 +3,8 @@ from flask import render_template,request
 from .requests import get_movies, get_movie_id
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    movie_selction = request.form.get('movies_option')
     data_link = get_movies('popular')
     return render_template('index.html', data_link= data_link)
 
